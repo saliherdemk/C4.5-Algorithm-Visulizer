6aacfcd603225d6c90ae4d6779498403cd6f3c22
Salih Erdem Kaymak <20060365@stu.omu.edu.tr>
2022-12-20 00:39:04 +0300

    table can be expand or shrink

--- js/generateTable.js
+++ js/generateTable.js
@@ -1,17 +1,29 @@
 // https://www.valentinog.com/blog/html-table/
 function generateTableHead(table, data) {
   let thead = table.createTHead();
-  thead.classList.add("text-xs", "text-gray-700", "uppercase", "bg-gray-50");
+  thead.classList.add("text-xs", "text-gray-700", "uppercase");
   let row = thead.insertRow();
   for (let key of data) {
     let th = document.createElement("th");
-    th.classList.add("py-3", "px-6");
+    th.classList.add("py-3", "px-6", "sticky", "top-0", "bg-gray-100");
     let text = document.createTextNode(key);
     th.appendChild(text);
     row.appendChild(th);
   }
 }
 
+const tableContainer = document.querySelector(".table-container");
+const btnTable = document.getElementById("btn-table");
+
+function toggleTable() {
+  if (btnTable.innerText === "Hide Table") {
+    tableContainer.style.maxHeight = "100px";
+    btnTable.innerText = "Show Table";
+  } else {
+    tableContainer.style.maxHeight = "1000px";
+    btnTable.innerText = "Hide Table";
+  }
+}
 function generateTable(table, data) {
   for (let element of data) {
     let row = table.insertRow();
